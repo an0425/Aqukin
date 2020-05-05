ad057bfcb2d2f16e8643caccf909c481082cafa7
Tom Le <42980704+tomle1452@users.noreply.github.com>
2020-05-04 23:20:01 -0500

    hide port and pw

--- src/lavalink.js
+++ src/lavalink.js
@@ -3,11 +3,11 @@ const fs = require('fs')
 let application = fs.readFileSync('./application.yml','utf8')
 
 if(process.env.PORT){
-	application = application.replace('2333',process.env.PORT)
+	application = application.replace('DYNAMICPORT',process.env.PORT)
 }
 
 if(process.env.PASS){
-	application = application.replace('minato',process.env.PASS)
+	application = application.replace('youshallnotpass',process.env.PASSWORD)
 }
 fs.writeFileSync('./application.yml', application)
 
