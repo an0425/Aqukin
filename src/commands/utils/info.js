b815f646729b4d5e2c22efae563d382b60c859e0
Tom Le <42980704+tomle1452@users.noreply.github.com>
2020-05-08 03:29:06 -0500

    Update info.js
    bug fixed, code clean up

--- src/commands/utils/info.js
+++ src/commands/utils/info.js
@@ -8,6 +8,9 @@ module.exports = class InfoCommand extends BaseCommand{
     
     async run(para) {
         const user = para.message.mentions.users.first(); // get the mentioned user
+        // checks if the author has mentioned anyone, if not return a message to inform the author
+        if (!user) { return para.message.channel.send(`**${para.message.author.user}**-sama, it looks like you haven't mentioned anyone~, please ensure that you are using the right syntax`, para.ridingAqua);}
+        
         // checks if the user has tagged Aqukin
         if(user.id === "702620458130079750"){
             const aquaCh = "https://www.youtube.com/channel/UC1opHUrw8rvnsadT-iGp7Cg";
@@ -32,21 +35,23 @@ module.exports = class InfoCommand extends BaseCommand{
         // if not continue with the code
         const member = para.message.guild.member(user); // get the mention user via the guild member list
         var memberRoles = member.roles.cache.map(role => role.name).join("\n") // get the mention user role(s) in the guild
-        if (member) { // if they are in the server
-            var nickname;
-            if(member.nickname) nickname = member.nickname; // checks if the user has a nickname
-            else nickname = "None"; // if not display "None" instead of "null"
-            const embed = new MessageEmbed()
-                .setColor(0x1DE2FE)
-                .setThumbnail("https://media1.tenor.com/images/cc14493a42cd23e7db5a45675089b87e/tenor.gif?itemid=17042819")
-                .setTitle(`**${para.message.author.username}**-sama, this is **${member.displayName}**-sama information`)
-                .addFields({name: "Nickname", value: nickname, inline: true},
-                           {name: "Tag", value: member.user.tag},
-                           {name: "Role(s)", value: memberRoles, inline: true},
-                           {name: "Date Joined", value: member.joinedAt.toLocaleDateString(), inline: true})
-                .setImage(member.user.displayAvatarURL({format: "png", dynamic: true, size: 2048}))
-                .setFooter("Vive La Résistance le Hololive~");
-            para.message.channel.send(embed)
-        } else { para.message.channel.send(`${para.message.author.user}-sama, it looks like the person you mentioned isn't in this guild~`, para.ridingAqua);} // else they are not in the server
+        // checks if the member is in the guild, if not return a message to inform the author
+        if (!member) { return para.message.channel.send(`**${para.message.author.user}**-sama, it looks like the person you mentioned isn't in this guild~`, para.ridingAqua);}
+        
+        var nickname;
+        if(member.nickname) nickname = member.nickname; // checks if the user has a nickname
+        else nickname = "None"; // if not display "None" instead of "null"
+        const embed = new MessageEmbed()
+            .setColor(0x1DE2FE)
+            .setThumbnail("https://media1.tenor.com/images/cc14493a42cd23e7db5a45675089b87e/tenor.gif?itemid=17042819")
+            .setTitle(`**${para.message.author.username}**-sama, this is **${member.displayName}**-sama information`)
+            .addFields({name: "Nickname", value: nickname, inline: true},
+                        {name: "Tag", value: member.user.tag},
+                        {name: "Role(s)", value: memberRoles, inline: true},
+                        {name: "Date Joined", value: member.joinedAt.toLocaleDateString(), inline: true})
+            .setImage(member.user.displayAvatarURL({format: "png", dynamic: true, size: 2048}))
+            .setFooter("Vive La Résistance le Hololive~");
+        para.message.channel.send(embed)
+        
     } // end of run
 }; // end of module.exports
