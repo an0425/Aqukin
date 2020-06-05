9e147abb261c636e8b97c761fd641608c14be628
Tom Le <42980704+tomle1452@users.noreply.github.com>
2020-06-05 02:35:45 -0500

    Update live_status.js

--- src/commands/utils/live_status.js
+++ src/commands/utils/live_status.js
@@ -3,7 +3,7 @@ const {ytNotify} = require("../../utilities/artificial_intelligence/youtube_noti
 const BaseCommand = require("../../utilities/structures/BaseCommand");
 
 module.exports = class LiveStatusCommand extends BaseCommand{
-    constructor() {super("livestatus", ["ls", "live", "streaming"], "Check the live status of a youtube channel", "SEND_MESSAGES", "utility", true, false, "<channel name/URL>")}
+    constructor() {super("liveinfo", ["li", "live", "streaming"], "Check the live status of a youtube channel", "SEND_MESSAGES", "utility", true, false, "<channel name/URL>")}
 
     async run(para) {
         ytNotify(para);
