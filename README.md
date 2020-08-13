45bce57e4d246e1fa88e34e228387c5b76f89667
Minh Duc Le <42980704+DeaLoux@users.noreply.github.com>
2020-08-13 03:12:45 -0500

    command spam check fix

--- README.md
+++ README.md
@@ -12,8 +12,8 @@ Your diamond ninja combat baka maid, a bot that was created based on a Virtual Y
 For the list of commands or more info on setting Aqukin up, please refer to the [Wiki pages](https://github.com/DeaLoux/Aqukin/wiki)
 
 # [License](https://github.com/DeaLoux/Aqukin/blob/master/LICENSE)
-- Character Copyright (c) 2017-2020 [Cover corp](https://cover-corp.com/)
-- Background Copyright (c) [Gaou-sensei](https://twitter.com/umaiyo_puyoman)
-- Software Copyright (c) 2020 Minh Duc Le / DeaLoux
-- Aqukin is licensed under the [MIT License](https://github.com/DeaLoux/Aqukin/blob/master/LICENSE)
+- `Character Copyright` (c) 2017-2020 [Cover corp](https://cover-corp.com/)
+- `Background Copyright` (c) [Gaou-sensei](https://twitter.com/umaiyo_puyoman)
+- `Software Copyright` (c) 2020 Minh Duc Le / DeaLoux
+- `Aqukin` is licensed under the [MIT License](https://github.com/DeaLoux/Aqukin/blob/master/LICENSE)
 - The code is open-source and free to use, please have a look at the license and keep in mind about giving appropriate credit where it is due, thank you.
--- src/events/message.js
+++ src/events/message.js
@@ -11,12 +11,6 @@ module.exports = class MessageEvent extends BaseEvent {
         // exclude messages sent by bots and direct messages
         if (message.author.bot || message.channel.type === "dm") { return; }
 
-        /* anti spam */
-        if(bot.antispam.has(message.author.id)) { 
-            return message.channel.send(`**${message.author.username}**-sama, you are rushing ${bot.user.username} hard, please be patient ＼(º □ º l|l)/`, { files: ["https://media1.tenor.com/images/9d81ec7c2abd005d8da208d2f56e89df/tenor.gif?itemid=17267165"] });
-        }
-        await spamCheck(bot, message);
-        
         // get the prefix
         let settings = await bot.settings.getSettings(message.guild.id);
         
@@ -25,36 +19,41 @@ module.exports = class MessageEvent extends BaseEvent {
         }
 
         // checks for command
-        typeCheck(bot, message, settings.prefix, bot.mentionCmd.tag)
-            .then(async (iscmd) => {
-                if(!iscmd){ // checks if the message is not a command
-                    const args = message.content.trim().split(/ +/g);
-                    if(settings.react) { await react(message, bot); }
-                    return;
-                } // end of if the message is not a command
+        typeCheck(bot, message, settings.prefix, bot.mentionCmd.tag).then(async (iscmd) => {
+            if(!iscmd){ // checks if the message is not a command
+                const args = message.content.trim().split(/ +/g);
+                if(settings.react) { await react(message, bot); }
+                return;
+            } // end of if the message is not a command
 
-                // else continue with the code
-                let args;
-                if(await bot.mentionCmd.mentioned.get(message.guild.id)) { args = message.content.slice(bot.mentionCmd.tag.length).trim().split(/ +/g); }
-                else { args = message.content.slice(settings.prefix.length).trim().split(/ +/g); }
-                if(!args) { return; }
-                const commandName = args[0].toLowerCase();
-                const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-                let para = await commandCheck(bot, message, command, args, settings.prefix, settings.reply);
+            /* anti spam */
+            if(bot.antispam.has(message.author.id)) { 
+                return message.channel.send(`**${message.author.username}**-sama, you are rushing ${bot.user.username} hard, please be patient ＼(º □ º l|l)/`, { files: ["https://media1.tenor.com/images/9d81ec7c2abd005d8da208d2f56e89df/tenor.gif?itemid=17267165"] });
+            }
+            await spamCheck(bot, message);
 
-                // checks if the parameters is returned, if not do nothing
-                if(!para) { 
-                    await bot.mentionCmd.mentioned.delete(message.guild.id);
-                    return; 
-                } 
+            // else continue with the code
+            let args;
+            if(await bot.mentionCmd.mentioned.get(message.guild.id)) { args = message.content.slice(bot.mentionCmd.tag.length).trim().split(/ +/g); }
+            else { args = message.content.slice(settings.prefix.length).trim().split(/ +/g); }
+            if(!args) { return; }
+            const commandName = args[0].toLowerCase();
+            const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+            let para = await commandCheck(bot, message, command, args, settings.prefix, settings.reply);
 
-                // try executing the command and catch any errors
-                command.run(para).catch((err) => {
-                    console.log(err); 
-                    message.channel.send(`**${message.author.username}**-sama, \`${err}\``);
-                });
+            // checks if the parameters is returned, if not do nothing
+            if(!para) { 
                 await bot.mentionCmd.mentioned.delete(message.guild.id);
-            })
-            .catch((err) => console.log(err));
+                return; 
+            } 
+
+            // try executing the command and catch any errors
+            command.run(para).catch((err) => {
+                console.log(err); 
+                message.channel.send(`**${message.author.username}**-sama, \`${err}\``);
+            });
+            await bot.mentionCmd.mentioned.delete(message.guild.id);
+        })
+        .catch((err) => console.log(err));
     } // end of run
 }// end of module.exports
